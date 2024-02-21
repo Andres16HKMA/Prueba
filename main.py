2fb398d05fc3822171feb2be0b62b924ea8fcb61
Andres16HKMA <kamzyx2004@gmail.com>
2024-02-21 07:17:48 -0500

    ParteA

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("hi world")
-print("segunda parte")
+import json
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+from azure.communication.email import EmailClient
+from 
+app = Flask(__name__)
+a=sudoku3x3.Cuadros()
